f96bc9d25c2d2ad494556a7029026062ba4c97b9
Yashwant Saste <yashwant.saste.ext@siemens.com>
2026-03-19 19:26:43 +0530

    Learning to init the variables in the rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+
+    //initializing an integer type of variable in the Rust..
+    let x :i32= 5;
+    let num =32;
+    println!("{}",num);
+    println!("{}", x);
+
+    //initializing a boolean type of variable in the Rust..
+    let is_odd = true;
+    if is_odd {
+        println!("The given number is odd");
+    }
+
+    //initializing a boolean type of variable in the Rust..
+    let str = String::from("Hello Rust!");
+    println!("{}",str);
+
+    // indexing a string 
+
+    let char1 = str.chars().nth(2);
+    match char1 {
+        Some(char1)=>println!("The char at the index is: {}",char1),
+        None=>println!("Out of bound!"),
+    }
 }
